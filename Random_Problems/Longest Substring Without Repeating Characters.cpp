ad01aa37c991e49e019ab2778554a154b50f0826
arsahdziban <arshadziban031201@gmail.com>
2025-06-20 18:21:53 +0600

    updated

--- Random_Problems/Longest Substring Without Repeating Characters.cpp
+++ Random_Problems/Longest Substring Without Repeating Characters.cpp
@@ -12,11 +12,14 @@ public:
         int maxLength = 0;
         
         for (int right = 0; right < s.size(); right++) {
+            // If the character is already in the set, move the left pointer
             while (charSet.find(s[right]) != charSet.end()) {
                 charSet.erase(s[left]);
                 left++;
             }
+            // Insert the current character into the set
             charSet.insert(s[right]);
+            // Update the maximum length found so far
             maxLength = max(maxLength, right - left + 1);
         }
         
