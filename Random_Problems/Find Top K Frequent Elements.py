2febc927c1cb3283b2a8199c909be7de39a18e14
arsahdziban <arshadziban031201@gmail.com>
2025-06-30 20:39:40 +0600

    initial commit

---
+++ Random_Problems/Find Top K Frequent Elements.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+def top_k_frequent(nums, k):
+   
+    count = Counter(nums)
+    
+   
+    most_common = count.most_common(k)
+    
+    
+    return [num for num, freq in most_common]
+
+# Test case
+nums = [1, 1, 1, 2, 2, 3]
+k = 2
+print(top_k_frequent(nums, k)) 
