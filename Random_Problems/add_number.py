d5e48202cdb8a098cad91296995a687337a6925e
arsahdziban <arshadziban031201@gmail.com>
2025-08-24 22:07:45 +0600

    initial commit

---
+++ Random_Problems/add_number.py
@@ -0,0 +1,8 @@
+def add_number():
+    user_input1 = input("Enter first number: ")
+    user_input2 = input("Enter second number: ")
+    
+    sum = float(user_input1) + float(user_input2)
+    print("The sum is:", sum)
+    
+add_number()
