c0012bc97a1ce8e29c3b33ee8766775db4fdddc3
arsahdziban <arshadziban031201@gmail.com>
2025-09-10 22:09:37 +0600

    initial commit

---
+++ Random_Problems/large_number.py
@@ -0,0 +1,10 @@
+x = int(input("Enter a large number: "))
+y = int(input("Enter another large number: "))
+
+
+if x > y:
+    print(f"{x} is larger than {y}")        
+elif x == y:
+    print(f"{x} is equal to {y}")
+else:
+    print(f"{y} is larger than {x}") 
