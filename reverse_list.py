80aa704ed251af94cea0cfb6d7ad90bcf6f8aa0e
arsahdziban <arshadziban031201@gmail.com>
2025-09-18 21:02:08 +0600

    updated

--- reverse_list.py
+++ reverse_list.py
@@ -1,8 +1,4 @@
-# Original list
+
 numbers = [10, 25, 7, 98, 35]
-
-# Reverse the list using slicing
 reversed_list = numbers[::-1]
-
-# Print the reversed list
 print("Reversed list:", reversed_list)
