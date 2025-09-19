a9c069b9e4d593cd546f1b70cd75a86670a7f3c6
arsahdziban <arshadziban031201@gmail.com>
2025-09-19 21:56:08 +0600

    initial commit

---
+++ triangle.py
@@ -0,0 +1,8 @@
+a = float(input("Enter the first side: "))
+b = float(input("Enter the second side: "))
+c = float(input("Enter the third side: "))
+
+if a + b > c and a + c > b and b + c > a:
+    print("These sides can form a triangle")
+else:
+    print("These sides cannot form a triangle")
