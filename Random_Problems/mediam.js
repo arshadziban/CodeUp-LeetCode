d42801b9d3318f13d97487336cfb824dcd00a83f
arshadziban <arshadziban031201@gmail.com>
2025-09-28 22:00:47 +0600

    initial commit

--- Random_Problems/mediam.js
+++ Random_Problems/mediam.js
@@ -33,6 +33,4 @@ function findMedianSortedArrays(nums1, nums2) {
 }
 
 console.log(findMedianSortedArrays([1,3], [2]));    
-console.log(findMedianSortedArrays([1,2], [3,4]));  
-console.log(findMedianSortedArrays([], [1]));       
-console.log(findMedianSortedArrays([0,0], [0,0]));  
+console.log(findMedianSortedArrays([1,2], [3,4])); 
