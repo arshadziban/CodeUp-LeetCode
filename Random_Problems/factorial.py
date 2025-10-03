e6dc5c88365c2646a77aad8642038e85f2617f81
arsahdziban <arshadziban031201@gmail.com>
2025-10-03 22:30:58 +0600

    updated

--- Random_Problems/factorial.py
+++ Random_Problems/factorial.py
@@ -1,12 +1,11 @@
-# Take input from user
+
 num = int(input("Enter a number: "))
 
-# Initialize factorial
 factorial = 1
 
-# Loop to calculate factorial
+
 for i in range(1, num + 1):
     factorial *= i
 
-# Print result
+
 print(num, "factorial is:", factorial)
