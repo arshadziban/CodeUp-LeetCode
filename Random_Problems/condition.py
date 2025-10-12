5f013001448488598c496094b9b780971fd4b89e
arsahdziban <arshadziban031201@gmail.com>
2025-10-12 22:41:37 +0600

    initial commit

---
+++ Random_Problems/condition.py
@@ -0,0 +1,14 @@
+def multiplication_or_sum(num1, num2):
+    product = num1 * num2
+    if product <= 1000:
+        return product
+    else:
+        return num1 + num2
+
+# Test with first example
+result = multiplication_or_sum(20, 30)
+print("The result is", result)
+
+# Test with second example
+result = multiplication_or_sum(40, 30)
+print("The result is", result)
