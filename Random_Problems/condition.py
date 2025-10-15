030725f94a9cea571630ec62fc3da9107322f43e
arsahdziban <arshadziban031201@gmail.com>
2025-10-15 23:31:25 +0600

    updated

--- Random_Problems/condition.py
+++ Random_Problems/condition.py
@@ -9,6 +9,6 @@ def multiplication_or_sum(num1, num2):
 result = multiplication_or_sum(20, 30)
 print("The result is", result)
 
-
+## result will be 600
 result = multiplication_or_sum(40, 30)
 print("The result is", result)
