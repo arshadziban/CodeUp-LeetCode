31be80695e750e3f21e083d23dfa524602e9a244
arsahdziban <arshadziban031201@gmail.com>
2025-10-17 22:43:21 +0600

    updated

--- Random_Problems/largest_number.py
+++ Random_Problems/largest_number.py
@@ -2,4 +2,4 @@ numbers = [10, 20, 4, 45, 99]
 unique_numbers = list(set(numbers))
 unique_numbers.sort()
 second_largest = unique_numbers[-2]
-print("The second largest number is", second_largest)
+print( second_largest)
